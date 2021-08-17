44462cafe61d70cda3bda369e5255d29b59e1e8f
greenhestu <greenhestu@gmail.com>
2021-08-17 18:04:30 +0900

    Add files via upload

---
+++ ch 8 NLP/TorchText_kr.py
@@ -0,0 +1,30 @@
+#하는 중...
+import urllib.request
+import pandas as pd
+from torchtext import data # torchtext.data 임포트
+from konlpy.tag import Mecab
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt", filename="ratings_train.txt")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt", filename="ratings_test.txt")
+
+train_df = pd.read_table('ratings_train.txt')
+test_df = pd.read_table('ratings_test.txt')
+print('훈련 데이터 샘플의 개수 : {}'.format(len(train_df)))
+print('테스트 데이터 샘플의 개수 : {}'.format(len(test_df)))
+
+tokenizer = Mecab()
+
+# 필드 정의
+ID = data.Field(sequential = False,
+                use_vocab = False) # 실제 사용은 하지 않을 예정
+
+TEXT = data.Field(sequential=True,
+                  use_vocab=True,
+                  tokenize=tokenizer.morphs, # 토크나이저로는 Mecab 사용.
+                  lower=True,
+                  batch_first=True,
+                  fix_length=20)
+
+LABEL = data.Field(sequential=False,
+                   use_vocab=False,
+                   is_target=True)
